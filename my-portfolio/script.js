0bd055bf97ccc698b4aa640451bffe125ebaa91e
rifathasan5575 <rifatronak5575@gmail.com>
2025-02-13 20:43:19 +0600

    Updated portfolio website

--- my-portfolio/script.js
+++ my-portfolio/script.js
@@ -1,3 +1,18 @@
-function showAlert() {
-    alert("Hello! Welcome to my portfolio.");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let text = "A Passionate Web Developer";
+    let index = 0;
+    let typingText = document.querySelector(".typing-text");
+
+    function typeEffect() {
+        if (index < text.length) {
+            typingText.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 100);
+        }
+    }
+
+    setTimeout(() => {
+        typingText.innerHTML = "";
+        typeEffect();
+    }, 1000);
+});
